c397fe76dd597d5d50284aca7547e53375fc2056
Viji <plviji.hotmail.com>
2019-07-29 19:45:06 -0400

    Composite Key added

--- cinemunch/src/main/java/com/cinemunch/controllers/OrderController.java
+++ cinemunch/src/main/java/com/cinemunch/controllers/OrderController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.cinemunch.controllers;
 
 import java.util.List;
@@ -41,51 +41,6 @@ public class OrderController {
 	}
 
 }
-=======
-package com.cinemunch.controllers;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.cinemunch.beans.Orders;
-import com.cinemunch.service.OrdersService;
-
-@RestController
-@CrossOrigin("*")
-@RequestMapping(value="/orders")
-public class OrderController {
-	
-	@Autowired
-	OrdersService service;
-	
-	@RequestMapping
-	public ResponseEntity<List<Orders>> findAll(){
-		List<Orders> orders = service.getAll();
-		if(orders == null || orders.size() == 0) return new ResponseEntity<List<Orders>>(HttpStatus.NO_CONTENT);
-		return new ResponseEntity<List<Orders>>(orders, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Orders> add(@RequestBody Orders orders){
-		orders = service.add(orders);
-		return new ResponseEntity<Orders>(orders, HttpStatus.CREATED);
-	}
-	
-//	@RequestMapping
-//	public ResponseEntity<List<seatId>> findAll(){
-//		List<seatId> orders = service.getAll();
-//		if(orders == null || orders.size() == 0) return new ResponseEntity<List<seatId>>(HttpStatus.NO_CONTENT);
-//		return new ResponseEntity<List<Orders>>(orders, HttpStatus.OK);
-//	}
-
-}
->>>>>>> 6c86bf47d39ce90e04001c8e6d2200440a62cd97
--- cinemunch/src/main/java/com/cinemunch/repositories/OrdersRepository.java
+++ cinemunch/src/main/java/com/cinemunch/repositories/OrdersRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.cinemunch.repositories;
 
 import java.util.List;
@@ -14,14 +14,4 @@ public interface OrdersRepository extends JpaRepository<Orders, Integer> {
 	List<Orders> findSeatIdByShowTime(int showTimeId);
 	
 }
-=======
-package com.cinemunch.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cinemunch.beans.Orders;
-
-public interface OrdersRepository extends JpaRepository<Orders, Integer> {
-	
-}
->>>>>>> 6c86bf47d39ce90e04001c8e6d2200440a62cd97
